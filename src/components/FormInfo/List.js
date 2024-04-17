0b82291557e3af9a3fac38a4a3896738f30aefce
zhipenglin <sunandmoon001@163.com>
2024-04-17 18:12:31 +0800

    添加FormInfo List的删除按钮传参

--- src/components/FormInfo/List.js
+++ src/components/FormInfo/List.js
@@ -7,143 +7,151 @@ import { GroupList, useFormContext } from "@kne/react-form-antd";
 import FormInfo from "./FormInfo";
 import classnames from "classnames";
 import importMessages from "./locale";
-import { FormattedMessage, IntlProvider } from "@components/Intl";
+import { createWithIntl, FormattedMessage, useIntl } from "@components/Intl";
 import get from "lodash/get";
 import style from "./style.module.scss";
 
-const List = ({
-  title,
-  addText,
-  minLength,
-  isUnshift,
-  maxLength,
-  defaultLength,
-  label,
-  name,
-  beforeAdd,
-  afterDelete,
-  column,
-  list,
-  listRender,
-  itemTitle,
-  outer,
-  renderChildren,
-  className,
-  important,
-}) => {
-  const groupRef = useRef(null);
-  const context = useFormContext();
-  const { formData } = context;
-  const allowAdd = !(
-    maxLength && maxLength <= get(formData, `${name}.length`, 0)
-  );
-  const addHandler = () => {
-    if (
-      typeof beforeAdd === "function"
-        ? beforeAdd(name, context, {
-            isUnshift,
-            group: groupRef.current,
-          }) !== false
-        : true
-    ) {
-      groupRef.current.onAdd({ isUnshift });
-    }
-  };
-  return (
-    <IntlProvider importMessages={importMessages} moduleName="FormInfo">
-      {cloneElement(
-        outer,
-        {
-          title,
-          addText,
-          className: classnames(className, style["list-part"]),
-          allowAdd: isUnshift && allowAdd,
-          add: addHandler,
-        },
-        renderChildren(
-          <GroupList name={name} defaultLength={defaultLength} ref={groupRef}>
-            {(...groupArgs) => {
-              const [key, { index, onRemove, length }] = groupArgs;
-              const formInfoProps = {
-                key,
-                column,
-                list:
-                  typeof list === "function"
-                    ? list(...groupArgs, context)
-                    : list,
-                title:
-                  typeof itemTitle === "function"
-                    ? itemTitle({
-                        index,
-                        key,
-                        onRemove,
-                      })
-                    : itemTitle,
-                groupArgs,
-                extra: (
-                  <FormattedMessage id="delText" moduleName="FormInfo">
-                    {(text) =>
-                      !(minLength && minLength >= length) ? (
-                        <ConfirmButton
-                          danger
-                          className={"btn-no-padding"}
-                          message={
-                            <FormattedMessage
-                              id="delConfirm"
-                              moduleName="FormInfo"
-                              values={{ name: label || title }}
-                            />
-                          }
-                          type="link"
-                          onClick={() => {
-                            onRemove(key);
-                            afterDelete && afterDelete(...groupArgs, context);
-                          }}
-                        >
-                          <Icon type="icon-shanchu" />
-                          {text}
-                        </ConfirmButton>
-                      ) : null
-                    }
-                  </FormattedMessage>
-                ),
-              };
-              return (
-                <div
-                  key={key}
-                  className={classnames(style["list-item"], {
-                    [style["is-important"]]: important,
-                  })}
-                >
-                  {typeof listRender === "function" ? (
-                    listRender(formInfoProps)
-                  ) : (
-                    <FormInfo
-                      {...formInfoProps}
-                      className={style["list-item-part"]}
-                      gap={16}
-                    />
-                  )}
-                  <Divider />
-                </div>
-              );
-            }}
-          </GroupList>
-        )
-      )}
-      {!isUnshift && allowAdd ? (
-        <FormattedMessage id="addText" moduleName="FormInfo">
-          {(text) => (
-            <Button onClick={addHandler}>
-              <Icon type="icon-tianjia" />
-              {addText || text}
-            </Button>
-          )}
-        </FormattedMessage>
-      ) : null}
-    </IntlProvider>
-  );
-};
+const List = createWithIntl({ moduleName: "FormInfo", importMessages })(
+  ({
+    title,
+    addText,
+    minLength,
+    isUnshift,
+    maxLength,
+    defaultLength,
+    label,
+    name,
+    beforeAdd,
+    afterDelete,
+    column,
+    list,
+    listRender,
+    itemTitle,
+    outer,
+    renderChildren,
+    className,
+    important,
+    deleteButtonProps,
+  }) => {
+    const groupRef = useRef(null);
+    const context = useFormContext();
+    const { formData } = context;
+    const allowAdd = !(
+      maxLength && maxLength <= get(formData, `${name}.length`, 0)
+    );
+    const { formatMessage } = useIntl({ moduleName: "FormInfo" });
+    const addHandler = () => {
+      if (
+        typeof beforeAdd === "function"
+          ? beforeAdd(name, context, {
+              isUnshift,
+              group: groupRef.current,
+            }) !== false
+          : true
+      ) {
+        groupRef.current.onAdd({ isUnshift });
+      }
+    };
+    return (
+      <>
+        {cloneElement(
+          outer,
+          {
+            title,
+            addText,
+            className: classnames(className, style["list-part"]),
+            allowAdd: isUnshift && allowAdd,
+            add: addHandler,
+          },
+          renderChildren(
+            <GroupList name={name} defaultLength={defaultLength} ref={groupRef}>
+              {(...groupArgs) => {
+                const [key, { index, onRemove, length }] = groupArgs;
+                const formInfoProps = {
+                  key,
+                  column,
+                  list:
+                    typeof list === "function"
+                      ? list(...groupArgs, context)
+                      : list,
+                  title:
+                    typeof itemTitle === "function"
+                      ? itemTitle({
+                          index,
+                          key,
+                          onRemove,
+                        })
+                      : itemTitle,
+                  groupArgs,
+                  extra: (
+                    <FormattedMessage id="delText" moduleName="FormInfo">
+                      {(text) =>
+                        !(minLength && minLength >= length) ? (
+                          <ConfirmButton
+                            danger
+                            className={"btn-no-padding"}
+                            message={formatMessage(
+                              { id: "delConfirm" },
+                              { name: label || title }
+                            )}
+                            type="link"
+                            onClick={() => {
+                              onRemove(key);
+                              afterDelete && afterDelete(...groupArgs, context);
+                            }}
+                            {...Object.assign({}, deleteButtonProps)}
+                          >
+                            {deleteButtonProps?.children ? (
+                              deleteButtonProps?.children
+                            ) : (
+                              <>
+                                <Icon type="icon-shanchu" />
+                                {text}
+                              </>
+                            )}
+                          </ConfirmButton>
+                        ) : null
+                      }
+                    </FormattedMessage>
+                  ),
+                };
+                return (
+                  <div
+                    key={key}
+                    className={classnames(style["list-item"], {
+                      [style["is-important"]]: important,
+                    })}
+                  >
+                    {typeof listRender === "function" ? (
+                      listRender(formInfoProps)
+                    ) : (
+                      <FormInfo
+                        {...formInfoProps}
+                        className={style["list-item-part"]}
+                        gap={16}
+                      />
+                    )}
+                    <Divider />
+                  </div>
+                );
+              }}
+            </GroupList>
+          )
+        )}
+        {!isUnshift && allowAdd ? (
+          <FormattedMessage id="addText" moduleName="FormInfo">
+            {(text) => (
+              <Button onClick={addHandler}>
+                <Icon type="icon-tianjia" />
+                {addText || text}
+              </Button>
+            )}
+          </FormattedMessage>
+        ) : null}
+      </>
+    );
+  }
+);
 
 const Outer = ({ add, addText, className, allowAdd, ...props }) => {
   return (
