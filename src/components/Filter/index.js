a8c8f4f7488091228ffcff46470d3cdcd873c289
zhipenglin <sunandmoon001@163.com>
2024-05-23 10:17:27 +0800

    修改filter导出方式

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,9 +1,17 @@
+import Filter from "./Filter";
+import * as fields from "./fields";
+import getFilterValue from "./getFilterValue";
+
+Filter.fields = fields;
+Filter.getFilterValue = getFilterValue;
+export default Filter;
+export { fields, getFilterValue };
+
 export { default as FilterValueDisplay } from "./FilterValueDisplay";
 export { default as FilterItem } from "./FilterItem";
 export { default as FilterLines } from "./FilterLines";
 export { default as PopoverItem } from "./PopoverItem";
 export { default as withFieldItem } from "./withFieldItem";
-export { default as getFilterValue } from "./getFilterValue";
 export { default as FilterItemContainer } from "./FilterItemContainer";
 export {
   InputFilterItem,
@@ -18,5 +26,3 @@ export {
   DateRangePickerFilterItem,
   TypeDateRangePickerFilterItem,
 } from "./fields";
-export * as fields from "./fields";
-export { default } from "./Filter";
