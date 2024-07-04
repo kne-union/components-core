b88427b48ead6f2ae8fce1106902bc3531ea4ff8
zhipenglin <sunandmoon001@163.com>
2024-07-04 15:52:08 +0800

    修改isEmpty判断规则

--- src/components/Table/isColValueEmpty.js
+++ src/components/Table/isColValueEmpty.js
@@ -2,7 +2,9 @@ const isColValueEmpty = (value, emptyOf) =>
   typeof emptyOf === "function"
     ? emptyOf(value)
     : (Array.isArray(value) && value.length === 0) ||
-      (typeof value === "object" && Object.values(value).length === 0) ||
+      (value &&
+        typeof value === "object" &&
+        Object.values(value).length === 0) ||
       value === "" ||
       value === void 0 ||
       value === null;
