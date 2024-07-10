53825d214339d49081c1a2fc18096a8d427855d1
zhipenglin <sunandmoon001@163.com>
2024-07-10 10:59:45 +0800

    修改office文件预览

--- src/components/FilePreview/OfficePreview.js
+++ src/components/FilePreview/OfficePreview.js
@@ -1,13 +1,14 @@
 import { usePreset } from "@components/Global";
 import { createWithFetch } from "@kne/react-fetch";
-import { typeFormatComponent } from "./typeFormat";
+import { typeFormatComponent, typeComponentMapping } from "./typeFormat";
 
 const OfficePreviewInner = createWithFetch({
   cache: "office-preview",
 })(({ data, className, ...props }) => {
   const { data: fileList } = data;
-  return fileList.map(({ url }) => {
-    const PreviewComponent = typeFormatComponent(url);
+  return fileList.map(({ url, type }) => {
+    const PreviewComponent =
+      typeComponentMapping[type] || typeFormatComponent(url);
     return (
       <PreviewComponent {...props} url={url} key={url} className={className} />
     );
@@ -15,12 +16,25 @@ const OfficePreviewInner = createWithFetch({
 });
 
 const OfficePreview = ({ id, apis: propsApis, className, ...props }) => {
-  const { apis: baseApis } = usePreset();
+  const { apis: baseApis, staticUrl, ajax } = usePreset();
   const apis = Object.assign({}, baseApis, propsApis);
   return (
     <OfficePreviewInner
       {...props}
-      {...apis.previewOffice}
+      {...(apis.previewOffice || {
+        loader: async ({ data }) => {
+          return {
+            data: [
+              {
+                url: `https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(
+                  (staticUrl || "") + apis.oss.url.replace("{id}", data.id)
+                )}&wdPrint=0&wdEmbedCode=0`,
+                type: "html",
+              },
+            ],
+          };
+        },
+      })}
       data={{ id }}
       className={className}
     />
