a882a2903a7098d3d90b62ce1f5bf6c2dbacfefd
zhipenglin <sunandmoon001@163.com>
2024-09-27 10:17:43 +0800

    修改FormStep的实现

--- src/components/FormInfo/FormStepModal.js
+++ src/components/FormInfo/FormStepModal.js
@@ -1,119 +1,196 @@
-import FormModal, { FormModalButton, useFormModal } from "./FormModal";
-import { Space, Steps } from "antd";
-import { useState } from "react";
-import {
-  CancelButton,
-  SubmitButton,
-  useFormContext,
-} from "@kne/react-form-antd";
+import Modal, { useModal } from "@components/Modal";
+import { useState, useRef } from "react";
+import classnames from "classnames";
+import { CancelButton, SubmitButton } from "@kne/react-form-antd";
+import { Form } from "./formModule";
+import FetchButton from "@common/components/FetchButton";
+import { IntlProvider, FormattedMessage } from "@components/Intl";
+import style from "./style.module.scss";
+import importMessages from "./locale";
+import { Button, Steps, Flex } from "antd";
 
-const StepsContent = ({
-  setCurrent,
-  current,
-  items,
-  setStepData,
-  stepData,
-}) => {
-  const currentItem = items[current];
-  const { formData } = useFormContext();
-  return (
-    <Space className="full-space" direction="vertical">
-      <Steps
-        current={current}
-        onChange={(current) => {
-          setStepData((stepData) => {
-            const newStepData = stepData.slice(0);
-            newStepData[current] = formData;
-            return newStepData;
-          });
-          setCurrent(current);
-        }}
-        items={items}
-      />
-      {currentItem.children}
-    </Space>
-  );
+const localeModuleName = "FormInfo";
+
+const FormStepState = ({ items, children }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const stepCacheRef = useRef({});
+  return children({
+    currentIndex,
+    setCurrentIndex,
+    stepCacheRef,
+    isLastStep: currentIndex === items.length - 1,
+    currentProps: Object.assign({}, items[currentIndex]),
+  });
 };
 
-const useSteps = () => {
-  const [stepData, setStepData] = useState([]);
-  const [current, setCurrent] = useState(0);
-
-  return ({ items, formProps }) => {
-    const isLastStep = current === items.length - 1;
-    return {
-      stepData,
-      formProps: Object.assign({}, formProps, {
-        data: Object.assign({}, formProps.data?.[current], stepData[current]),
-        onSubmit: (data) => {
-          const newStepData = stepData.slice(0);
-          newStepData[current] = data;
-          setStepData(newStepData);
-          if (isLastStep) {
-            return formProps?.onSubmit(stepData);
-          }
-          setCurrent(current + 1);
-        },
-      }),
-      children: (
-        <StepsContent
-          items={items}
-          current={current}
-          setCurrent={setCurrent}
-          stepData={stepData}
-          setStepData={setStepData}
-        />
-      ),
-      footerButtons: [
-        { children: "取消", ButtonComponent: CancelButton },
-        {
-          type: "primary",
-          children: !isLastStep ? "下一步" : "保存",
-          ButtonComponent: SubmitButton,
-          autoClose: false,
-        },
-      ],
-    };
+const computedCommonProps = ({
+  className,
+  withDecorator,
+  cancelText,
+  nextText,
+  completeText,
+  items,
+  ...modalProps
+}) => {
+  return {
+    ...modalProps,
+    footerButtons: ({ currentIndex, isLastStep }) => {
+      const { footerButtons } = Object.assign({}, items[currentIndex]);
+      return (
+        footerButtons || [
+          {
+            children: (
+              <IntlProvider
+                importMessages={importMessages}
+                moduleName={localeModuleName}
+              >
+                {cancelText || (
+                  <FormattedMessage
+                    id={"Cancel"}
+                    moduleName={localeModuleName}
+                  />
+                )}
+              </IntlProvider>
+            ),
+            ButtonComponent: CancelButton,
+          },
+          {
+            type: "primary",
+            children: (
+              <IntlProvider
+                importMessages={importMessages}
+                moduleName={localeModuleName}
+              >
+                {isLastStep
+                  ? completeText || (
+                      <FormattedMessage
+                        id={"Complete"}
+                        moduleName={localeModuleName}
+                      />
+                    )
+                  : nextText || (
+                      <FormattedMessage
+                        id={"Next"}
+                        moduleName={localeModuleName}
+                      />
+                    )}
+              </IntlProvider>
+            ),
+            ButtonComponent: SubmitButton,
+            autoClose: false,
+          },
+        ]
+      );
+    },
+    className: classnames(className, style["form-outer"], style["form-modal"]),
+    withDecorator: (render) => {
+      const innerRender = (props) => {
+        return (
+          <FormStepState items={items}>
+            {({
+              currentIndex,
+              isLastStep,
+              setCurrentIndex,
+              currentProps,
+              stepCacheRef,
+            }) => {
+              const { formProps, title, ...otherProps } = currentProps;
+              const { onSubmit, ..._formProps } =
+                typeof formProps === "function" ? formProps(props) : formProps;
+              return (
+                <Form
+                  {..._formProps}
+                  onSubmit={async (data, ...args) => {
+                    stepCacheRef.current[currentIndex] = { data };
+                    const res =
+                      onSubmit &&
+                      (await onSubmit(
+                        data,
+                        Object.assign(
+                          {},
+                          {
+                            currentIndex,
+                            isLastStep,
+                            setCurrentIndex,
+                            currentProps,
+                            stepCacheRef,
+                          }
+                        ),
+                        ...args
+                      ));
+                    if (!isLastStep && res !== false) {
+                      setCurrentIndex((currentIndex) => currentIndex + 1);
+                      return;
+                    }
+                    if (isLastStep && res !== false) {
+                      modalProps?.onClose();
+                    }
+                  }}
+                  key={currentIndex}
+                >
+                  <IntlProvider
+                    importMessages={importMessages}
+                    moduleName="FormInfo"
+                  >
+                    {render(
+                      Object.assign({}, props, otherProps, {
+                        currentIndex,
+                        isLastStep,
+                        setCurrentIndex,
+                        stepSection: (
+                          <Steps
+                            className={style["form-step-section"]}
+                            current={currentIndex}
+                            items={items.map(({ title }) => ({ title }))}
+                          />
+                        ),
+                      })
+                    )}
+                  </IntlProvider>
+                </Form>
+              );
+            }}
+          </FormStepState>
+        );
+      };
+      return typeof withDecorator === "function"
+        ? withDecorator(innerRender)
+        : innerRender();
+    },
+    children: ({ children, stepSection, ...props }) => {
+      return (
+        <Flex vertical gap={24}>
+          <Flex justify="center">{stepSection}</Flex>
+          {typeof children === "function" ? children(props) : children}
+        </Flex>
+      );
+    },
   };
 };
 
 const FormStepModal = (props) => {
-  const createSteps = useSteps();
-  const { footerButtons, children, formProps } = createSteps(props);
-  return (
-    <FormModal {...props} formProps={formProps} footerButtons={footerButtons}>
-      {children}
-    </FormModal>
-  );
+  return <Modal {...computedCommonProps(props)} />;
 };
 
 export default FormStepModal;
 
 export const useFormStepModal = () => {
-  const formModal = useFormModal();
-  const createSteps = useSteps();
-  return (props) => {
-    const { footerButtons, children, formProps } = createSteps(props);
-    return formModal({
-      ...props,
-      formProps,
-      footerButtons,
-      children,
-    });
-  };
+  const modal = useModal();
+  return (props) => modal(computedCommonProps(props));
 };
 
-export const FormStepModalButton = ({ modalProps, ...props }) => {
-  const createSteps = useSteps();
-  const { footerButtons, children, formProps } = createSteps(modalProps);
-  return (
-    <FormModalButton
-      {...props}
-      modalProps={Object.assign({}, modalProps, {
-        formProps,
-        footerButtons,
-        children,
-      })}
-    />
-  );
+export const FormStepModalButton = (props) => {
+  const formModal = useFormStepModal();
+  if (!props.api) {
+    const { modalProps, ...others } = props;
+    return (
+      <Button
+        {...others}
+        onClick={() => {
+          formModal(modalProps);
+        }}
+      />
+    );
+  }
+  return <FetchButton {...props} modalFunc={formModal} />;
 };
