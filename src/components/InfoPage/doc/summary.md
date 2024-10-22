5cb9241a3099466bc792f99ea0aed43d1e99327d
zhipenglin <sunandmoon001@163.com>
2024-10-22 15:27:39 +0800

    提交一下

--- src/components/InfoPage/doc/summary.md
+++ src/components/InfoPage/doc/summary.md
@@ -1,9 +1,17 @@
-### 何时使用
+### *`InfoPage`* 何时使用
 
-一般用在复杂的详情展示页面，InfoPage提供了一个标准的展示信息的格式
+一般用在复杂的详情展示页面，`InfoPage` 提供了一个标准的展示信息的格式
 
 ### 特点
 
-* 支持Content组件Descriptions组件的组合
-* 支持Collapse组件组合
-* InfoPage.Part 需要放在InfoPage之下，InfoPage.Collapse,Content,Descriptions 需要放在 InfoPage.Part之下
+* 支持 `Content` 组件 `Descriptions` 组件的组合
+* 支持 `Collapse` 组件组合
+* `InfoPage.Part`、`InfoPage.Collapse` 需要放在 `InfoPage` 之下，`Content`、`Descriptions` 可以任意组合
+
+### *`Content`* 何时使用
+
+成组展示多个字段，常见于详情页的信息展示
+
+### 特点
+
+labelAlign 不为 auto 时会自动计算 label 的最小宽度使所有 label 的宽度等于最长的 label 宽度，使视觉上更加整齐有秩序感
--- src/components/InfoPage/doc/table-view.js
+++ src/components/InfoPage/doc/table-view.js
@@ -1,85 +1,127 @@
 const { TableView } = _InfoPage;
+const { Flex } = antd;
+const { useState } = React;
+
+const dataSource = [
+  {
+    id: "RC00101",
+    name: "张三",
+    birthday: "2020-03-03",
+    addTime: new Date(),
+    count: 2000.1322,
+    count2: 0.01234565,
+    count3: 1234523,
+    description: `描述描述描述描述描述描述描述描述`,
+  },
+  {
+    id: "RC00102",
+    name: "李四",
+    birthday: "2020-03-03",
+    addTime: new Date(),
+    count: 2000.1322,
+    count2: 0.01234565,
+    count3: 1234523,
+    description: `描述描述描述描述描述描述描述描述`,
+  },
+  {
+    id: "RC00103",
+    name: "王五",
+    birthday: "2020-03-03",
+    addTime: new Date(),
+    count: 2000.1322,
+    count2: 0.01234565,
+    count3: 1234523,
+    description: `描述描述描述描述描述描述描述描述`,
+  },
+  {
+    id: "RC00104",
+    name: "马七",
+    birthday: "2020-03-03",
+    addTime: new Date(),
+    count: 2000.1322,
+    count2: 0.01234565,
+    count3: 1234523,
+    description: `描述描述描述描述描述描述描述描述`,
+  },
+];
+
+const columns = [
+  {
+    name: "id",
+    title: "ID",
+  },
+  {
+    name: "name",
+    title: "姓名",
+  },
+  {
+    name: "birthday",
+    title: "出生日期",
+    format: "date",
+  },
+  {
+    name: "addTime",
+    title: "添加时间",
+    format: "datetime",
+  },
+  {
+    name: "count",
+    title: "数量",
+    format: "number",
+  },
+  {
+    name: "description",
+    title: "描述",
+    span: 10,
+  },
+];
+
+const WithCheckbox = () => {
+  const [selectKeys, setSelectKeys] = useState([]);
+  return (
+    <TableView
+      dataSource={dataSource}
+      columns={columns}
+      rowSelection={{
+        type: "checkbox",
+        allowSelectedAll: true,
+        selectedRowKeys: selectKeys,
+        onChange: setSelectKeys,
+      }}
+    />
+  );
+};
+
+const WithSelected = () => {
+  const [selectKeys, setSelectKeys] = useState([]);
+  return (
+    <TableView
+      dataSource={dataSource}
+      columns={columns}
+      rowSelection={{
+        selectedRowKeys: selectKeys,
+        onChange: setSelectKeys,
+      }}
+    />
+  );
+};
 
 const BaseExample = () => {
   return (
-    <TableView
-      dataSource={{
-        id: "RC00101",
-        name: "张三",
-        birthday: "2020-03-03",
-        addTime: new Date(),
-        count: 2000.1322,
-        count2: 0.01234565,
-        count3: 1234523,
-        description: `描述描述描述描述描述描述描述描述\n描述描述描述描述描述描述描述描述\n描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述`,
-        description2: `描述描述描述描述描述描述描述描述`,
-      }}
-      col={3}
-      columns={[
-        {
-          name: "id",
-          title: "ID",
-          block: true,
-        },
-        {
-          name: "name",
-          title: "姓名",
-          span: 10,
-        },
-        {
-          name: "birthday",
-          title: "出生日期",
-          format: "date",
-        },
-        {
-          name: "addTime",
-          title: "添加时间",
-          format: "datetime",
-        },
-        {
-          name: "count",
-          title: "数量",
-          format: "number",
-        },
-        {
-          name: "count2",
-          title: "百分比",
-          format: "number-percent money-百分比",
-        },
-        {
-          name: "count3",
-          title: "万元",
-          format: "number--10000",
-          render: (value) => `${value}万元`,
-        },
-        {
-          name: "empty",
-          title: "空值显示",
-        },
-        {
-          name: "empty2",
-          title: "空值显示2",
-          placeholder: "空",
-        },
-        {
-          name: "empty3",
-          title: "空值显示3",
-          emptyIsPlaceholder: false,
-        },
-        {
-          name: "description",
-          title: "描述",
-        },
-        {
-          name: "description2",
-          title: "描述",
-        },
-        {
-          name: "end",
-          title: "尾行优化",
-        },
-      ]}
-    />
+    <Flex vertical gap={10}>
+      <TableView dataSource={dataSource} columns={columns} />
+      <WithCheckbox />
+      <WithSelected />
+      <TableView dataSource={[]} columns={columns} />
+      <div
+        style={{
+          height: "200px",
+          overflowY: "scroll",
+        }}
+      >
+        <TableView dataSource={dataSource} columns={columns} sticky />
+      </div>
+    </Flex>
   );
 };
 
