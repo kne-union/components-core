3be4081f8c5f2daf575767fbcc19070c2c989596
zhipenglin <zhipeng.lin@leapin.io>
2025-01-15 09:57:42 +0800

    修改问题

--- src/components/FormInfo/fields/Upload/index.js
+++ src/components/FormInfo/fields/Upload/index.js
@@ -1,130 +1,133 @@
-import { Space } from "antd";
+import {Space} from "antd";
 import classnames from "classnames";
 import useControlValue from "@kne/use-control-value";
 import style from "./style.module.scss";
-import { List as FileList } from "@components/File";
-import { hooks } from "@kne/react-form-antd";
+import {List as FileList} from "@components/File";
+import {hooks} from "@kne/react-form-antd";
+import {createWithIntl, useIntl} from '@components/Intl';
 import omit from "lodash/omit";
-import { InputFileButton, useFileUpload } from "@common/hocs/withInputFile";
+import {InputFileButton, useFileUpload} from "@common/hocs/withInputFile";
+import importMessages from "@components/FormInfo/locale";
 
-const { useOnChange } = hooks;
+const {useOnChange} = hooks;
 
-const UploadField = ({
-  className,
-  fileSize,
-  maxLength,
-  multiple,
-  size,
-  accept,
-  children,
-  renderTips,
-  showUploadList,
-  onSave,
-  ossUpload,
-  onUpload,
-  getPermission,
-  ...props
-}) => {
-  const [propsValue, onChange] = useControlValue(props);
-  const value = propsValue || [];
-  const { fileList: uploadingList, onFileSelected } = useFileUpload({
-    multiple,
-    onSave,
-    onUpload: ossUpload || onUpload,
-    fileSize,
-    maxLength,
-    value,
-    onChange,
-    concurrentCount: 10,
-  });
-  const previewFileList = [...uploadingList, ...value];
+const UploadField = createWithIntl({
+    importMessages, moduleName: 'FormInfo'
+})((p) => {
+    const {formatMessage} = useIntl({moduleName: "FormInfo"});
+    const {
+        className,
+        fileSize,
+        maxLength,
+        multiple,
+        size,
+        accept,
+        children,
+        renderTips,
+        showUploadList,
+        onSave,
+        ossUpload,
+        onUpload,
+        getPermission,
+        ...props
+    } = Object.assign({}, {
+        defaultValue: [],
+        children: formatMessage({id: 'uploadButtonText'}),
+        accept: [
+            ".pdf",
+            ".jpg",
+            ".png",
+            ".jpeg",
+            ".doc",
+            ".docx",
+            ".xls",
+            ".xlsx",
+            ".html",
+            ".msg",
+            ".eml",
+            ".zip",
+        ],
+        renderTips: (defaultTips) => {
+            return defaultTips;
+        },
+        multiple: true,
+        showUploadList: true,
+        maxLength: 10,
+        fileSize: 30,
+    }, p);
+    const [propsValue, onChange] = useControlValue(props);
+    const value = propsValue || [];
+    const {fileList: uploadingList, onFileSelected} = useFileUpload({
+        multiple,
+        onSave,
+        onUpload: ossUpload || onUpload,
+        fileSize,
+        maxLength,
+        value,
+        onChange,
+        concurrentCount: 10,
+    });
+    const previewFileList = [...uploadingList, ...value];
+    const tipsText = renderTips(formatMessage({id: 'uploadTips'}, {
+            extensionNames: accept
+                .map((str) => str.replace(/^\./, ""))
+                .join(","), maxLength, fileSize
+        }),
+        {
+            fileSize,
+            maxLength,
+            accept,
+        }
+    );
 
-  const tipsText = renderTips(
-    `支持扩展名${accept
-      .map((str) => str.replace(/^\./, ""))
-      .join("、")},单个文件大小不超过${fileSize}M，最多上传${maxLength}个附件`,
-    {
-      fileSize,
-      maxLength,
-      accept,
-    }
-  );
-
-  return (
-    <Space direction="vertical">
-      <Space>
-        <InputFileButton
-          {...omit(props, ["value", "onChange"])}
-          size={size}
-          multiple={multiple}
-          accept={accept}
-          className={classnames(style["upload"], className)}
-          onChange={onFileSelected}
-        >
-          {children}
-        </InputFileButton>
-        {tipsText && <div className={style["tips"]}>{tipsText}</div>}
-      </Space>
-      {showUploadList && previewFileList.length > 0 && (
-        <FileList
-          className={style["upload-list"]}
-          dataSource={previewFileList}
-          infoItemRenders={[]}
-          getPermission={
-            getPermission
-              ? getPermission
-              : (type) => {
-                  return type === "delete";
-                }
-          }
-          apis={{
-            onDelete: (target) => {
-              const newList = value.slice(0);
-              const index = newList.indexOf(target);
-              index > -1 && newList.splice(index, 1);
-              onChange(newList);
-            },
-          }}
-        />
-      )}
-    </Space>
-  );
-};
-
-UploadField.defaultProps = {
-  defaultValue: [],
-  // icon:  <Icon type="icon-shangchuanfujian" />,
-  children: "上传附件",
-  accept: [
-    ".pdf",
-    ".jpg",
-    ".png",
-    ".jpeg",
-    ".doc",
-    ".docx",
-    ".xls",
-    ".xlsx",
-    ".html",
-    ".msg",
-    ".eml",
-    ".zip",
-  ],
-  renderTips: (defaultTips) => {
-    return defaultTips;
-  },
-  multiple: true,
-  showUploadList: true,
-  maxLength: 10,
-  fileSize: 30,
-};
+    return (
+        <Space direction="vertical">
+            <Space>
+                <InputFileButton
+                    {...omit(props, ["value", "onChange"])}
+                    size={size}
+                    multiple={multiple}
+                    accept={accept}
+                    className={classnames(style["upload"], className)}
+                    onChange={onFileSelected}
+                >
+                    {children}
+                </InputFileButton>
+                {tipsText && <div className={style["tips"]}>{tipsText}</div>}
+            </Space>
+            {showUploadList && previewFileList.length > 0 && (
+                <FileList
+                    className={style["upload-list"]}
+                    dataSource={previewFileList}
+                    infoItemRenders={[]}
+                    getPermission={
+                        getPermission
+                            ? getPermission
+                            : (type) => {
+                                return type === "delete";
+                            }
+                    }
+                    apis={{
+                        onDelete: (target) => {
+                            const newList = value.slice(0);
+                            const index = newList.indexOf(target);
+                            index > -1 && newList.splice(index, 1);
+                            onChange(newList);
+                        },
+                    }}
+                />
+            )}
+        </Space>
+    );
+});
 
 const Upload = (props) => {
-  const render = useOnChange(props);
-  return render(UploadField);
+    const render = useOnChange(props);
+    return render(UploadField);
 };
 
 Upload.defaultProps = {
-  interceptor: "file-format",
+    interceptor: "file-format",
 };
 
 Upload.Field = UploadField;
